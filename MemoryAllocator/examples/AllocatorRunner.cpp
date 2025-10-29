7702bd5d62f6a91c0f793142cef59b1676a891fb
Tim V <tim.vasenkov@gmail.com>
2025-10-29 15:30:19 -0500

    Add example folder

---
+++ MemoryAllocator/examples/AllocatorRunner.cpp
@@ -0,0 +1,20 @@
+#include "Allocator.hpp"
+#include <iostream>
+int main()
+{
+  Allocator alloc(8192);
+  int** test1 = (int**)alloc.malloc(5);
+  int** test2 = (int**)alloc.malloc(11);
+  int** test3 = (int**)alloc.malloc(16);
+  int** test4 = (int**)alloc.malloc(27);
+  int** test5 = (int**)alloc.malloc(43);
+  alloc.free(*test2);
+  alloc.free(*test4);
+  alloc.printChunks();
+  // Keep track of all free chunks and implement defragment to automatically be called
+
+  // Maybe we could get rid of the prev variable in the chunks and in the functions
+  // since its not used for anything and is just bloating overhead and reducing readability.
+  // maybe rewrite chunksize to be a size_t
+
+};
