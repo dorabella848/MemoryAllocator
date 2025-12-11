0243f18c6c7055d730b22e8bd8335421e1bf8612
Tim V <tim.vasenkov@gmail.com>
2025-12-11 15:00:53 -0600

    Remove dependency on Allocator.cpp and connect to Allocator.tpp
    Change library to header only format to comply with STL allocator format which needs Allocator to be a template class

--- MemoryAllocator/include/Allocator.hpp
+++ MemoryAllocator/include/Allocator.hpp
@@ -1,28 +1,52 @@
+#include "Chunk.hpp"
 #include <new>
 #include <cstdint>
-struct Chunk;
+#include <cstdio>
+#include <cmath> 
+#include <cstring>
+#include <iostream>
+#include <stdexcept>
 
+#pragma once
 template <typename T>
 class Allocator {
-    T value;
     private:
         int memorySize;
         int freeMemory;
         uint8_t* memoryPool;
-        Chunk* occHead = nullptr;
-        Chunk* freeHead = nullptr;
+        Chunk<T>* occHead = nullptr;
+        Chunk<T>* freeHead = nullptr;
         
     public:
-        Allocator(const T& value, std::size_t numBytes);
+        using value_type = T;
+        Allocator();
         ~Allocator();
-        Chunk* getFreeHead();
-        Chunk* getOccHead();
+        Allocator(std::size_t size);
+        // defailt copy constructor
+        Allocator(const Allocator& other);
+        // changed type copy constrcu
+        template <typename U>
+        Allocator(const Allocator<U>& other);
+
+        Chunk<T>* getFreeHead();
+
+        Chunk<T>* getOccHead();
+
         int getFreeMemory();
+
         int getMemoryTotal();
-        T* getMemAddress(std::size_t index);
+
+        T* getMemAddress(size_t index);
+
         void printChunks();
-        T* malloc(std::size_t size);
-        void free(T* ptr);
-        T* calloc(std::size_t number, std::size_t size);
-        T* realloc(T* ptr, std::size_t size);
+
+        T* allocate(size_t size);
+
+        void deallocate(T* ptr);
+        // STL-required overload
+        void deallocate(T* p, std::size_t n);
+
+        T* calloc(size_t number, size_t size);
+        T* reallocate(T* ptr, size_t size);
 };
+#include "Allocator.tpp"
