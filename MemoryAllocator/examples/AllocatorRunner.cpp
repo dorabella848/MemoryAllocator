c3604dfa22f593aaa281a02e69a98960f5a7e878
Tim V <tim.vasenkov@gmail.com>
2025-12-11 15:09:40 -0600

    Update runner so it doesn't brick the build

--- MemoryAllocator/examples/AllocatorRunner.cpp
+++ MemoryAllocator/examples/AllocatorRunner.cpp
@@ -3,14 +3,20 @@
 #include <vector>
 int main()
 {
-  Allocator alloc(8192);
+
+  Allocator<int> alloc(8096);
+  int* chunk = alloc.allocate(10);
+  chunk = (int*)alloc.reallocate(alloc.getOccHead()->startLoc, 0);
+
+  alloc.printChunks();
+  
   
   std::vector<int> vec1 = {10,20,30};
   for (auto x: vec1) std::cout << x << " ";
   std::cout << '\n';
 
   // explicitly using custom allocator
-  std::vector<int, Allocator> vec2 = {10,20,30};
+  std::vector<int, Allocator<int>> vec2 = {10,20,30};
   for (auto x: vec2) std::cout << x << " ";
   std::cout << '\n';
 
