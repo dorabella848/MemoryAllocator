cf1bf9719b0b7dd727dd787a6eb5d8a1a6d07e36
timVcs50 <tim.vasenkov@gmail.com>
2026-01-22 14:55:58 -0600

    Implement orphan function
    Disconects a chunk's next and prev associations

--- MemoryAllocator/include/STL_Allocator/Chunk.hpp
+++ MemoryAllocator/include/STL_Allocator/Chunk.hpp
@@ -25,6 +25,19 @@ struct Chunk {
     prev(nullptr),
     AbsNext(nullptr),
     AbsPrev(nullptr) {};
+
+    void orphan()
+    {
+        this->next = nullptr;
+        this->prev = nullptr;
+        if(this->prev != nullptr){
+            this->prev->next = this->next;
+        }
+        if(this->next != nullptr){
+            this->next->prev = this->prev;
+        }
+    }
+
     ~Chunk() {
 
     }
