f85cd577ff274757ca37df743a4c3e99f024f034
timVcs50 <tim.vasenkov@gmail.com>
2026-01-29 16:59:09 -0600

    Add doxygen comments for all functions. Add option to not input a value for n when using Allocate()
    Remove the need to input a value for the n paramater in the STL Allocator's allocate() because it doesn't currently have any use when interacting with the memory pool.

--- MemoryAllocator/include/STL_Allocator/Allocator.hpp
+++ MemoryAllocator/include/STL_Allocator/Allocator.hpp
@@ -35,15 +35,55 @@ class Allocator {
     public:
         Allocator(std::size_t numBytes);
         ~Allocator();
+        /*
+        * Returns a Chunk* that points to the first free chunk.
+        */
         Chunk* getFreeHead();
+        /*
+        * Returns a Chunk* that points to the first occupied chunk.
+        */
         Chunk* getOccHead();
+        /*
+        * Returns a Chunk* that points to the chunk that occurs at the start of the memory pool.
+        */
         Chunk* getTrueHead();
+        /*
+        * Returns the remaining free memory.
+        */
         std::size_t getFreeMemory();
+        /*
+        * Returns the total memory reserved for the memory pool.
+        */
         std::size_t getMemoryTotal();
+        /* 
+        * Returns a void* to an index within the memory pool.
+        *
+        * This is a testing function that can be used to ensure that the 
+        * startIndex var stored within a chunk alligns with startLoc var for a chunk.
+        */
         void* getMemAddress(std::size_t index);
+        /*
+        * Prints the current memory pool's statistics and the chunk's connections, postions, and size 
+        * in order of occurence within the memory pool to the console.
+        */
         void printChunks();
+        /*
+        * Allocates a chunk of {size} bytes and returns a void* to the chunk.
+        */
         void* malloc(std::size_t size);
+        /*
+        * Unreserves the memory for a given chunk in the memory pool.
+        */
         void free(void* ptr);
+        /*
+        * Allocates a chunk of {number * size} bytes, sets the newly reserved memory to 0, 
+        * and returns a void* to the chunk.
+        */
         void* calloc(std::size_t number, std::size_t size);
+        /*
+        * Changes the number of reserved bytes for a given chunk. This may cause the position of 
+        * the reserved chunk to move so it returns a void* the chunk regardless of if it was
+        * moved or not.
+        */
         void* realloc(void* ptr, std::size_t size);
 };
